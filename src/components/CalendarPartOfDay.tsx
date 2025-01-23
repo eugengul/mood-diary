3cbba8538122467a7e187bad18497784d0cb422b
Evgenii <170091238+eugengul@users.noreply.github.com>
2025-01-23 11:58:53 +0100

    Fix calendar layout for long notes

--- src/components/CalendarPartOfDay.tsx
+++ src/components/CalendarPartOfDay.tsx
@@ -1,7 +1,8 @@
 import { Mood } from "@/constants/Moods";
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { MoodIcon } from "./MoodIcon";
+import { COLORS } from "@/constants/colors";
 
 const MAX_NOTE_LENGTH = 20;
 
@@ -11,16 +12,29 @@ const truncateNote = (note: string, maxLength: number = MAX_NOTE_LENGTH) =>
 export default function CalendarPartOfDay({ mood }: { mood: Mood | null }) {
   const { moodId, note } = mood || {};
   return (
-    <>
-      <MoodIcon mood={moodId || null} size={40} />
-      {note ? <Text style={styles.note}>{truncateNote(note)}</Text> : null}
-    </>
+    <View style={styles.container}>
+      <View style={styles.icon}>
+        <MoodIcon mood={moodId || null} size={40} />
+      </View>
+      <Text style={styles.note}>{note && truncateNote(note)}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  icon: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   note: {
+    flex: 1,
     opacity: 0.5,
     textAlign: "center",
+    borderTopWidth: 1,
+    borderColor: COLORS.MUTED_COLOR,
   },
 });
